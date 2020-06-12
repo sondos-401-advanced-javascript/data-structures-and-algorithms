c3561b7a419031ff1c4a5400ecb7dd99d7c65568
sondoshassan <sondos199628@gmail.com>
2020-06-12 21:47:57 +0300

    finish code

---
+++ challenges/fifoAnimalShelter/Readme.md
@@ -0,0 +1,19 @@
+# fifo-animal-shelter
+
+
+## Challenge
+Create a class called AnimalShelter which holds only dogs and cats. The shelter operates using a first-in, first-out approach. this class has two methods enqueue(animal) adds animal to the shelter. animal can be either a dog or a cat object. dequeue(pref) returns either a dog or a cat. If pref is not "dog" or "cat" then return null.
+
+## Approach & Efficiency
+- I did the test by using this command `npm test`
+
+## links
+- [pull request](https://github.com/sondos-401-advanced-javascript/data-structures-and-algorithms/pull/16)
+
+- [github actions](https://github.com/sondos-401-advanced-javascript/data-structures-and-algorithms/actions)
+
+## Big O
+time is O(n) for BinaryTree which n is number of node but the time for BinarySearchTree is O(log n). **Space**: O(h) for(BinaryTree and BinarySearchTree) which h is the high of tree.
+
+## Solution
+![Tree whiteboard](../../assets/tree.jpg)
--- challenges/fifoAnimalShelter/fifo-animal-shelter.js
+++ challenges/fifoAnimalShelter/fifo-animal-shelter.js
@@ -1,19 +1,69 @@
 'use strict';
 
+
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue{
+  constructor(){
+    this.storage = new Array();
+    this.front = null;
+  }
+  enqueue(value){
+    if(!this.front){
+      let node = new Node(value);
+      this.storage.push(node);
+      this.front = this.storage[0];
+    }
+    else{
+      let node = new Node(value);
+      let len = this.storage.length;
+      this.storage[len-1].next = len +1;
+      this.storage.push(node);
+      this.front = this.storage[0];  
+    }
+        
+  }
+}
+
 class AnimalShelter{
   constructor(){
     this.storage = new Array();
+    this.cats = new Queue();
+    this.dogs = new Queue();
   }
   enqueue(animal){
-    if(animal === 'dog' || animal === 'cat'){
-      this.storage.unshift(animal);
+    let newAnimal = new Node(animal);
+    if(animal === 'cat'){
+      this.cats.enqueue(newAnimal);
+      this.storage.unshift(this.cats.storage);
     }
+    else if(animal === 'dog'){
+      this.dogs.enqueue(newAnimal);
+      this.storage.unshift(this.dogs.storage);
+    }
+    
   }
+
   dequeue(pref){
-    if(pref === 'cat' || pref === 'dog'){
-      this.storage.pop();
+    if(pref==='cat'){
+      if(!this.cats.storage.length)return null;
+      let deleted = this.cats.storage.pop();
+      return deleted;
+    }
+    else if(pref==='dog'){
+      if(!this.dogs.storage.length)return null;
+      let deleted = this.dogs.storage.pop();
+      return deleted;
     }
     else return null;
   }
 
 }
+
+module.exports = AnimalShelter;
+
